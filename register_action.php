e95f4ef622bd64ff1eae687a1a686e229677a01c
Manilha7 <a58604@ualg.pt>
2019-11-03 23:33:17 +0000

    Update register_action.php

--- register_action.php
+++ register_action.php
@@ -21,7 +21,7 @@ include 'db.php';
         die("Erro " . mysql_errno() . " : " . mysql_error());
     
     $nrows  = mysql_num_rows($result);
-    if (empty($password) && empty($password_corfirmed) && empty($username)!=true && empty($email)!=true) {
+    if (empty($password) || empty($password_corfirmed) && empty($username)!=true && empty($email)!=true) {
       header("Location: register.php?Error=3&email=$email&username=$username"); 
     }
     elseif ( empty($password) || empty($password_corfirmed) || empty($username) || empty($email)) {
