93ecbaa14b1a8120991f49e30346e9e3b26206e6
Manilha7 <a58604@ualg.pt>
2019-11-03 23:20:35 +0000

    Update register_action.php

--- register_action.php
+++ register_action.php
@@ -36,9 +36,6 @@ include 'db.php';
       header("Location: register.php?Error=4&username=$username&email=$email");
     }
 
-    if (empty($password) && empty($password_corfirmed) && empty($username)!=true && empty($email)!=true) {
-      header("Location: register.php?Error=3&email=$email&username=$username"); 
-    }
     else{
         $sql_insert = "INSERT INTO users(name, email, password_digest, created_at, updated_at) VALUES('$username','$email','$password_final',NOW(),NOW())";
         if(!($result = @ mysql_query($sql_insert,$db)))
