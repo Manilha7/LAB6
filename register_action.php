07f3615c9fcc3cd15fc71460128828a3da3d15bb
Manilha7 <a58604@ualg.pt>
2019-11-03 23:06:38 +0000

    Update register_action.php

--- register_action.php
+++ register_action.php
@@ -21,7 +21,11 @@ include 'db.php';
         die("Erro " . mysql_errno() . " : " . mysql_error());
     
     $nrows  = mysql_num_rows($result);
-    if ( empty($password) || empty($password_corfirmed) || empty($username) || empty($email)) {
+
+    if (empty($password) && empty($password_corfirmed)) {
+      header("Location: register.php?Error=3&email=$email&username=$username"); 
+    }
+    elseif ( empty($password) || empty($password_corfirmed) || empty($username) || empty($email)) {
        header("Location: register.php?Error=0");
     }
 
@@ -33,9 +37,7 @@ include 'db.php';
       header("Location: register.php?Error=4&username=$username&email=$email");
     }
 
-    elseif ((empty($password) && empty($password_corfirmed)) && !=((empty($password) || empty($password_corfirmed))) {
-      header("Location: register.php?Error=3&email=$email&username=$username"); 
-    }
+    
 
         else{
         $sql_insert = "INSERT INTO users(name, email, password_digest, created_at, updated_at) VALUES('$username','$email','$password_final',NOW(),NOW())";
