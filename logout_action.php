66078f83c8b0c97ecbc6e96163d299269a7e1e6f
Manilha7 <a58604@ualg.pt>
2019-11-04 16:31:30 +0000

    Update logout_action.php

--- logout_action.php
+++ logout_action.php
@@ -1,9 +1,17 @@
 <?php
 
 include 'db.php';
+
+require('/usr/share/php/smarty/libs/Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty->template_dir = 'templates';
+$smarty->compile_dir = 'templates_c';
+
 session_start();
 session_unset();
 session_destroy();
 $Message= "See you back soon!";
+$smarty->assign("Message",$Message);
  header("Location: message_template.tpl&Message=$Message");
 ?>
