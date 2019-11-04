1bb17b8d3af3b084788be72d07a9b494f3a26023
Manilha7 <a58604@ualg.pt>
2019-11-04 01:13:09 +0000

    Update login_action.php

--- login_action.php
+++ login_action.php
@@ -5,15 +5,6 @@
 include 'db.php';
 
 
-    require_once('/usr/share/php/smarty/libs/Smarty.class.php');
-    $smarty = new Smarty();
-
-
-
-    $smarty->template_dir = 'templates';
-    $smarty->compile_dir = 'templates_c';
-
-
     // Process signup submission
     $db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
 
@@ -26,30 +17,36 @@ include 'db.php';
     
           
     $sql = "SELECT * FROM users WHERE email='$email'";
-    $sql1 = "SELECT * FROM users WHERE password_digest='$password'";
+    $sql1 = "SELECT email FROM users WHERE email='$email'";
+    $sql2 = "SELECT password_digest FROM users WHERE email='$email'";
+   
 
-    $result=mysql_query($sql,$db);
+     mysql_query($sql,$db);
+    $result = mysql_query($sql,$db);
     $dbdata = mysql_fetch_array($result);
-
-    $result1=mysql_query($sql,$db);
+    $dbexist = mysql_num_rows($result);
+    
+    $result1 = mysql_query($sql1,$db);
     $dbdata1 = mysql_fetch_array($result1);
+    $dbexist1 = mysql_num_rows($result1);
+    
+    $result2 = mysql_query($sql2,$db);
+    $dbdata2 = mysql_fetch_array($result2);
+    $dbexist2 = mysql_num_rows($result2);
 
-    $nrows  = mysql_num_rows($result);
-    $nrows1  = mysql_num_rows($result1);
-    if ($nrows==0 || $nrows1==0) {
-        $_SESSION["error"] = -1;
-        header("Location: login_template.tpl");
-    }
-    else {
+
+    //sucesso
+    if($dbdata['email']==$email && $dbdata['password_digest']==$password){
         $_SESSION["id"] = $dbdata['id'];
         $_SESSION["name"] = $dbdata['name'];
         $_SESSION["error"] = 0;
-        $smarty->assign("MENU_1","Home");
-        $smarty->assign("MENU_2","Logout");
-        $smarty->assign("MENU_3",$name);
         header("Location: index.php");
     }
-    
+    // insucesso
+     else if(($dbdata1['email']==$email && $dbdata2['password_digest']!=$password) || $dbexist1==0 ){
+        $_SESSION["error"] = -1;
+        header("Location: login.php?email=$email");
+    }
     }
 
 
