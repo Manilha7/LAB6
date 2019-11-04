b59cba10656ff32c33a24da2dbe354f91d67d22c
Manilha7 <a58604@ualg.pt>
2019-11-04 16:42:21 +0000

    Update logout_action.php

--- logout_action.php
+++ logout_action.php
@@ -13,5 +13,5 @@ session_unset();
 session_destroy();
 $Message= "See you back soon!";
 $smarty->assign("Message",$Message);
-header("Location: message_template.tpl&Message=$Message");
+$smarty->display('message_template.tpl')
 ?>
