3f99830b95d10ac09eddda3174fd217aa9f21c93
Manilha7 <a58604@ualg.pt>
2019-11-04 14:16:58 +0000

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ $smarty->assign("href2","register.php");
 $smarty->assign("href3","login.php");
 
 
-if ($Message eq 0) {
+if ($MESSAGE eq "Sucesso") {
 	$smarty->assign("MENU_2","Welcome".$nome);
 	$smarty->assign("MENU_3","Logout");
 	$smarty->assign("href2","");
