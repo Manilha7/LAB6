bdd196ee230c1f5505623a091220c401df076ac5
Manilha7 <a58604@ualg.pt>
2019-11-04 22:21:51 +0000

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ $smarty->assign("href2","register.php");
 $smarty->assign("href3","login.php");
 
 $name=$_SESSION['name'];
-if (isset($name)) {
+if (isset($_SESSION['name'])) {
 	$smarty->assign("MENU_2","Welcome ".$name);
 	$smarty->assign("MENU_3","Logout");
 	$smarty->assign("MENU_4","Post Blog");
